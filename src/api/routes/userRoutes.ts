24b6c30ca3c99fe5e86f5b2d1da73aa703442594
Peter-Binate <peter.binate@gmail.com>
2024-10-09 16:44:40 +0200

    add login

--- src/api/routes/userRoutes.ts
+++ src/api/routes/userRoutes.ts
@@ -1,8 +1,8 @@
 import express from 'express'
-import { register } from '../controllers/userControllers'
+import { register, login } from '../controllers/userControllers'
 const router = express.Router()
 
 router.post('/register', register)
-// router.post('/login', login)
+router.post('/login', login)
 
 export default router
